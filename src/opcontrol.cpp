50e360b2c7d3cf4fa3cf2e2af89cab42d32c58b7
Tad Keller <logischgl@gmail.com>
2019-06-04 09:09:10 -0500

    added vaquita image, changed a bit of CAD stuff

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include"display/lvgl.h"
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
@@ -13,19 +13,30 @@
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+pros::Motor rightFront(13, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rightBack(11, pros::E_MOTOR_GEARSET_18, true);
 
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
+pros::Motor leftFront(14, pros::E_MOTOR_GEARSET_18, false);
+pros::Motor leftBack(12, pros::E_MOTOR_GEARSET_18, false);
+
+pros::Controller con(pros::E_CONTROLLER_MASTER);
+
+void opcontrol() 
+{
+	extern const lv_img_t vaquita;
+	std::cout<<vaquita.header.w;
+	lv_obj_t * im = lv_img_create(lv_scr_act(), NULL);
+	lv_img_set_src(im, &vaquita);
+	lv_obj_set_pos(im, 0, 0);
+	lv_obj_set_drag(im, true);
+	//std::cerr<<"hi";
+	while(true)
+	{
+		rightFront.move(con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+		rightBack.move(con.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+
+		leftFront.move(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+		leftBack.move(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+		pros::Task::delay(10);
 	}
 }
