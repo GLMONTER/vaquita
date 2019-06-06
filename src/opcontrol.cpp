3b66c14f6a920c774103929ac6cf4a091056e7e2
Tad Keller <logischgl@gmail.com>
2019-06-05 23:47:05 -0500

    added lift code and life motor brake mods, removed default comments and did basic code cleanup

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,34 +1,55 @@
 #include "main.h"
 #include"display/lvgl.h"
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-pros::Motor rightFront(13, pros::E_MOTOR_GEARSET_18, true);
-pros::Motor rightBack(11, pros::E_MOTOR_GEARSET_18, true);
+
+pros::Motor rightFront(10, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rightBack(1, pros::E_MOTOR_GEARSET_18, true);
 
 pros::Motor leftFront(14, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor leftBack(12, pros::E_MOTOR_GEARSET_18, false);
 
+
+pros::Motor leftLift(10, pros::E_MOTOR_GEARSET_36, false);
+pros::Motor rightLift(15, pros::E_MOTOR_GEARSET_36, true);
+
 pros::Controller con(pros::E_CONTROLLER_MASTER);
+
+//a function that polls the controller for lift related controlls
+void pollLife()
+{
+	//if the left top buttton, move lift down
+	if(con.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && !con.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+	{
+		leftLift.move(-127);
+		rightLift.move(-127);
+	}
+	else
+	//if the right top button, move lift up
+	if(!con.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && con.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+	{
+		leftLift.move(127);
+		rightLift.move(127);
+	}
+	else
+	//if no life related input, make the life hold it's position, (Hold brake mode)
+	if(!con.get_digital(pros::E_CONTROLLER_DIGITAL_L1) && !con.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
+	{
+		leftLift.move_velocity(0);
+		rightLift.move_velocity(0);
+	}
+}
 
 void opcontrol() 
 {
+	//load the vaquita image from vaquita.c
 	extern const lv_img_t vaquita;
-	std::cout<<vaquita.header.w;
 	lv_obj_t * im = lv_img_create(lv_scr_act(), NULL);
 	lv_img_set_src(im, &vaquita);
 	lv_obj_set_pos(im, 0, 0);
 	lv_obj_set_drag(im, true);
+
+	//set the lift to hold it's current position when stopped
+	leftLift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	rightLift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 
 	while(true)
 	{
