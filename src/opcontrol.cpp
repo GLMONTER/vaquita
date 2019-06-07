8a9047eef2d649f17a9b95886187f63af0a44ba8
Tad Keller <logischgl@gmail.com>
2019-06-06 21:44:18 -0500

    switching distros, make sure there is nothing unsaved

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,15 +1,15 @@
 #include "main.h"
 #include"display/lvgl.h"
 
-pros::Motor rightFront(10, pros::E_MOTOR_GEARSET_18, true);
-pros::Motor rightBack(1, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rightFront(5, pros::E_MOTOR_GEARSET_18, true);
+pros::Motor rightBack(6, pros::E_MOTOR_GEARSET_18, true);
 
 pros::Motor leftFront(14, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor leftBack(12, pros::E_MOTOR_GEARSET_18, false);
 
 
-pros::Motor leftLift(10, pros::E_MOTOR_GEARSET_36, false);
-pros::Motor rightLift(15, pros::E_MOTOR_GEARSET_36, true);
+pros::Motor leftLift(1, pros::E_MOTOR_GEARSET_36, false);
+pros::Motor rightLift(10, pros::E_MOTOR_GEARSET_36, true);
 
 pros::Controller con(pros::E_CONTROLLER_MASTER);
 
@@ -41,11 +41,17 @@ void pollLife()
 void opcontrol() 
 {
 	//load the vaquita image from vaquita.c
+	/* 
 	extern const lv_img_t vaquita;
 	lv_obj_t * im = lv_img_create(lv_scr_act(), NULL);
 	lv_img_set_src(im, &vaquita);
 	lv_obj_set_pos(im, 0, 0);
 	lv_obj_set_drag(im, true);
+	*/
+
+	lv_obj_t* label = lv_label_create(lv_scr_act(), NULL);
+	lv_label_set_text(label, "test");
+	lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
 
 	//set the lift to hold it's current position when stopped
 	leftLift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -59,6 +65,9 @@ void opcontrol()
 		leftFront.move(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
 		leftBack.move(con.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
 
+		std::string temp  = std::to_string(leftLift.get_temperature());
+		lv_label_set_text(label, temp.c_str());
+		pollLife();
 		//se we can update the lvgl thread
 		pros::Task::delay(10);
 	}
