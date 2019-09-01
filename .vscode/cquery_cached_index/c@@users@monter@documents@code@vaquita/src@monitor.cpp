25f80bc432bd3ae45fa7c9512798cecbe9df811b
Tad Keller <logischgl@gmail.com>
2019-09-01 01:17:09 -0500

    added random stuff

---
+++ .vscode/cquery_cached_index/c@@users@monter@documents@code@vaquita/src@monitor.cpp
@@ -0,0 +1,32 @@
+/*#include<main.h>
+
+
+
+static void printError(std::string motorName, int temp)
+{
+  if(temp == 55)
+  {
+  //  controller.print(0,0, std::string(motorName + "OVERHEAT" + "%d").c_str(), temp);
+  }
+}
+
+void startMonitor()
+{
+  if(rightFront.get_temperature() > 50)
+  {
+    printError("Right Front", rightFront.get_temperature());
+  }
+  if(rightBack.get_temperature() > 50)
+  {
+    printError("Right Back", rightBack.get_temperature());
+  }
+  if(leftFront.get_temperature() > 50)
+  {
+    printError("Left Front", leftFront.get_temperature());
+  }
+  if(leftBack.get_temperature() > 50)
+  {
+    printError("Left Back", leftBack.get_temperature());
+  }
+}
+*/
