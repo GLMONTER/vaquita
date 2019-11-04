a68cdecad7f7593e9137e770e31f664b4265f338
Tad Keller <logischgl@gmail.com>
2019-11-03 23:17:06 -0600

    idk

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include "main.h"
-
-#include "main.h"
 #include"display/lvgl.h"
-//#include<display>
 
 
 static bool buttonPressed = false;
@@ -19,6 +16,7 @@ pros::Motor leftFront(13, pros::E_MOTOR_GEARSET_18, false);
 pros::Motor leftLift(11, pros::E_MOTOR_GEARSET_36, false);
 pros::Motor rightLift(20, pros::E_MOTOR_GEARSET_36, true);
 
+//slide mover
 pros::Motor liftRot(14, pros::E_MOTOR_GEARSET_36, true);
 
 pros::Motor loaderRot(16, pros::E_MOTOR_GEARSET_36, true);
@@ -47,7 +45,7 @@ void controllerPoll()
 		controller.rumble("- -");
 	}
 	isTime += 10;
-	time+= 5;
+	time += 5;
 	if(time == 500)
 	{
 	sens2.set_value(true);
@@ -61,7 +59,7 @@ void controllerPoll()
 //a function that polls the controller for lift related controlls
 void pollLift()
 {
-		static bool slow = false;
+	static bool slow = false;
 	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN))
 		slow = true;
 	else
@@ -253,35 +251,10 @@ void initialize()
 	loaderRot.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void red()
 {
 	loaderRot.move(127);
