eef6791f7e15cc18c0afd102afa567acf202f2fc
Tad Keller <logischgl@gmail.com>
2019-11-09 06:33:21 -0800

    shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "main.h"
 #include"display/lvgl.h"
 
-
+//button toggle variables
 static bool buttonPressed = false;
 static bool buttonToggle = false;
 
+//MOTORS
 const static pros::Motor rightBack(10, pros::E_MOTOR_GEARSET_18, true);
 const static pros::Motor rightFront(9, pros::E_MOTOR_GEARSET_18, true);
 
@@ -15,24 +16,26 @@ const static pros::Motor leftBack(20, pros::E_MOTOR_GEARSET_18, false);
 const static pros::Motor leftLift(12, pros::E_MOTOR_GEARSET_36, false);
 const static pros::Motor rightLift(2, pros::E_MOTOR_GEARSET_36, true);
 
-//slide mover
-const static pros::Motor liftRot(1, pros::E_MOTOR_GEARSET_36, true);
+//slide rotation
+const static pros::Motor slideRot(1, pros::E_MOTOR_GEARSET_36, true);
+//loader rotation
 const static pros::Motor loaderRot(11, pros::E_MOTOR_GEARSET_36, true);
 
+//main controller object
 static pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-
+//chassis controller
 static ChassisControllerIntegrated chassis = ChassisControllerFactory::create(
   {19, 20}, {-9, -10},
   AbstractMotor::gearset::green,
-  {4.25_in, 11.5_in}
+  {3_in, 10.25_in}
 );
 
+//time variables for rumble sequence
+static int isTime = 0;
+static int time = 0;
 
-int isTime = 0;
-int time = 0;
-
-void controllerPoll()
+static void controllerPoll()
 {
 	if(isTime == 45000)
 	{
@@ -52,8 +55,8 @@ void controllerPoll()
 
 }
 
-//a function that polls the controller for lift related controlls
-void pollLift()
+//a function that polls the controller for all mechanism related input like the loaders, slide, and arms.
+static void pollLift()
 {
 	static bool slow = false;
 	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN))
@@ -102,10 +105,10 @@ void pollLift()
 	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
 	{
 		if(!slow)
-			liftRot.move(127);
+			slideRot.move(127);
 		else
 		{
-			liftRot.move(60);
+			slideRot.move(60);
 		}
 
 	}
@@ -113,16 +116,16 @@ void pollLift()
 	if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
 	{
 		if(!slow)
-			liftRot.move(-127);
+			slideRot.move(-127);
 		else
 		{
-			liftRot.move(-60);
+			slideRot.move(-60);
 		}
 	}
 	else
 	if(!controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2) && !controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2) && !buttonToggle)
 	{
-		liftRot.move_velocity(0);
+		slideRot.move_velocity(0);
 	}
 
 
@@ -140,27 +143,6 @@ void pollLift()
 	{
 		loaderRot.move_velocity(0);
 	}
-}
-
-void set2()
-{
-	liftRot.tare_position();
-	//liftRot.set_zero_position(liftRot.get_position());
-	while(liftRot.get_position() >-2600)
-	{
-		liftRot.move(-60);
-	}
-	liftRot.move_velocity(0);
-	pros::Task::delay(1000);
-	chassis.setMaxVelocity(15);
-
-	leftLift.move(-90);
-	rightLift.move(-90);
-
-	chassis.moveDistance(-1.5_ft);
-	leftLift.move(0);
-	rightLift.move(0);
-
 }
 
 void opcontrol()
@@ -182,7 +164,7 @@ void opcontrol()
 		rightFront.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
 		leftFront.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
 
-		std::string temp  = "Lift Temp : " + std::to_string(liftRot.get_position());
+		std::string temp  = "Lift Temp : " + std::to_string(slideRot.get_position());
 		lv_label_set_text(label, temp.c_str());
 
 		pollLift();
@@ -201,20 +183,13 @@ void opcontrol()
 
 		if(buttonToggle)
 		{
-			liftRot.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+			slideRot.move(controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
 		}
 		controllerPoll();
 		pros::Task::delay(10);
 	}
 }
 
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize()
 {
 	loaderRot.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
@@ -224,7 +199,34 @@ void disabled() {}
 
 void competition_initialize() {}
 
-void red()
+//aton function for actually stacking the cubes
+static void stack()
+{
+  //zero the slide position
+	slideRot.tare_position();
+	while(slideRot.get_position() >-2600)
+	{
+		slideRot.move(60);
+	}
+	slideRot.move_velocity(0);
+
+	pros::Task::delay(1000);
+	chassis.setMaxVelocity(15);
+
+	leftLift.move(-90);
+	rightLift.move(-90);
+
+	chassis.moveDistance(-1.5_ft);
+	leftLift.move(0);
+	rightLift.move(0);
+}
+
+static void skills()
+{
+
+}
+
+static void red()
 {
 	loaderRot.move(127);
 	chassis.setMaxVelocity(50);
@@ -253,41 +255,51 @@ void red()
 	rightLift.move(0);
 
 
-	set2();
+	stack();
 }
 
-void blue()
+static void blue()
 {
-	loaderRot.move(127);
-	chassis.setMaxVelocity(50);
+	chassis.setMaxVelocity(30);
 
-	chassis.moveDistanceAsync(10_in);
-	pros::Task::delay(1000);
-	loaderRot.move(-127);
-	pros::Task::delay(1100);
-	loaderRot.move_velocity(0);
+	//chassis.moveDistanceAsync(10_in);
 
-	leftLift.move(127);
+  loaderRot.move(127);
+  pros::Task::delay(1500);
+  loaderRot.move(-127);
+
+  leftBack.move(-127);
+  rightBack.move(-127);
+  pros::Task::delay(500);
+  leftBack.move(0);
+  rightBack.move(0);
+
+  pros::Task::delay(900);
+  loaderRot.move_velocity(0);
+
+
+
+  chassis.setMaxVelocity(60);
+  leftLift.move(127);
 	rightLift.move(127);
-	chassis.setMaxVelocity(45);
-	chassis.moveDistance(26_in);
+  chassis.moveDistanceAsync(40_in);
 
 	chassis.setMaxVelocity(90);
-
-	chassis.moveDistance(-17_in);
-	chassis.turnAngle(-135_deg);
-	chassis.setMaxVelocity(35);
-	chassis.moveDistance(13_in);
-	leftLift.move(-80);
-	rightLift.move(-80);
-	pros::Task::delay(200);
-	leftLift.move(0);
+  chassis.waitUntilSettled();
+  leftLift.move(0);
 	rightLift.move(0);
+	chassis.moveDistance(-17_in);
+  chassis.setMaxVelocity(70);
+
+	chassis.turnAngle(-138_deg);
+	chassis.setMaxVelocity(70);
+	chassis.moveDistance(18_in);
 
 
-	set2();
+
+	stack();
 }
 void autonomous()
 {
-	red();
+	blue();
 }
